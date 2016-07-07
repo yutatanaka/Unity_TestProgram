769dbca91f44e6892f004a43f267dbd6d5217615
yutatanaka <koryotennis99@gmail.com>
2016-07-07 21:52:10 +0900

    カメラの追従、キャラクターのジャンプと移動、重力と床との当たり判定を実装

---
+++ Assets/Script/Player_CameraMove/PlayerMove.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour 
+{
+	// 移動速度
+	public float speed = 15.0F;
+
+	// ジャンプ速度
+	public float jumpSpeed = 8.0F;
+
+	// 重力の設定
+	public float gravity = 20.0F;
+
+	// ベクトルの方向
+	private Vector3 moveDirection = Vector3.zero;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		// プレイヤーの移動
+		CharacterController controller = GetComponent<CharacterController>();
+
+		if (controller.isGrounded) 
+		{
+			moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			moveDirection = transform.TransformDirection (moveDirection);
+			moveDirection *= speed;
+
+			if (Input.GetButton ("Jump")) 
+			{
+				moveDirection.y = jumpSpeed;
+			}
+		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move (moveDirection * Time.deltaTime);
+	}
+}

c03a22327c5d1c194a9d3a0fdf88ca452dfc6664
yutatanaka <koryotennis99@gmail.com>
2016-07-07 14:46:17 +0900

    カメラをスムーズに追従を実装

---
+++ Assets/Assets/Script/Camera_Test/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour 
+{
+
+	public Transform target;
+	public float smoothing = 3f;
+	Vector3 offset;
+
+	// Use this for initialization
+	void Start () 
+	{
+		offset = transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 targetCameraPosition = target.position + offset;
+		transform.position = Vector3.Lerp (transform.position, targetCameraPosition, smoothing * Time.deltaTime);
+	}
+}
---
+++ Assets/Assets/Script/Camera_Test/MoveBox.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBox : MonoBehaviour 
+{
+
+	public float speed = 0.05f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		float x = Input.GetAxis ("Horizontal");
+		float z = Input.GetAxis ("Vertical");
+		transform.Translate (x * speed, 0, z * speed);
+	}
+}
